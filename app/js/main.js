d391dbd6a7de250d36fff72bf5b66f69f8fb982b
Safiolla <yerkebulan.safiolla@gmail.com>
2023-08-12 18:17:42 +0600

    header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,32 @@ $('#searchForm').keyup(function (e) {
   }
 })
 
+//toggle mobile menu
+$('.menu-toggle').click(function () {
+  const btn = $('.menu-toggle-burger');
+  const mobileMenu = $('.header-bottom-row')
+  btn.toggleClass('active');
+  if(btn.hasClass('active')) {
+    mobileMenu.addClass('show')
+  } else {
+    mobileMenu.removeClass('show')
+  }
+  if(window.innerWidth < 768) {
+    const headerHeight = $('.header').height();
+    const windowHeight = window.innerHeight;
+    mobileMenu.css({
+      top: headerHeight,
+      height: windowHeight - headerHeight,
+    })
+  }
+})
+
+//toggle search on mobile
+$('.toggle-search').click(function () {
+  $('.search-container').addClass('show');
+})
+$('.close-search').click(function () {
+  $('.search-container').removeClass('show');
+  $('#searchForm').val('');
+  $('.search-result').removeClass('show')
+})
